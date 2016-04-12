b742b473f49c4e0ad1a6d6ee5046f3266d8ad6f4
Azatbek Altynekov <azatbek.09.94@gmail.com>
2016-04-13 03:51:25 +0600

    dsfsd

---
+++ js/custom.js
@@ -0,0 +1,51 @@
+/*!
+ * Start Bootstrap - Grayscale Bootstrap Theme (http://startbootstrap.com)
+ * Code licensed under the Apache License v2.0.
+ * For details, see http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+// jQuery to collapse the navbar on scroll
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
+
+   //Isotope
+	$(window).load(function(){
+		$portfolio = $('.location-items');
+		$portfolio.isotope({
+			itemSelector : 'li',
+			layoutMode : 'fitRows',
+			 cellsByRow : {
+				columnWidth : 240,
+				rowHeight : 360
+			  }
+		});
+		$portfolio_selectors = $('.location-filter>li>a');
+		$portfolio_selectors.on('click', function(){
+			$portfolio_selectors.removeClass('active');
+			$(this).addClass('active');
+			var selector = $(this).attr('data-filter');
+			$portfolio.isotope({ filter: selector });
+			return false;
+		});
+	});
---
+++ submit.php
@@ -0,0 +1,95 @@
+﻿<?php 
+if(isset($_POST['OrderToDiscount'])){
+	 /* Здесь проверяется существование переменных */
+	if (isset($_POST['dep_date'])) {$dep_date = $_POST['dep_date'];}
+	if (isset($_POST['arriv_date'])) {$arriv_date = $_POST['arriv_date'];}
+	if (isset($_POST['userName'])) {$name = $_POST['userName'];}
+	if (isset($_POST['phoneNumber'])) {$phone = $_POST['phoneNumber'];}
+
+	$type = "Заявка на получение скидки";
+	/* Сюда впишите свою эл. почту */
+	$address = "info@discoverytour.kz";
+
+	/* А здесь прописывается текст сообщения, \n - перенос строки */
+	$mes = "Тема: $type\nТелефон: $phone\nИмя: $name\nДата выезда: $dep_date\nДата приезда: $arriv_date";
+
+	/* А эта функция как раз занимается отправкой письма на указанный вами email */
+	$sub='Заявка на получение скидки;';//сабж
+	$email='<discoverytour.kz>'; // от кого
+	$send = mail($address,$sub,$mes,"Content-type:text/plain; charset = utf-8\r\nFrom:$email");
+	header("location:/");
+}
+
+if(isset($_POST['OrderToReserv'])){
+	/* Здесь проверяется существование переменных */
+	if (isset($_POST['name'])) {$name = $_POST['userName'];}
+	if (isset($_POST['phoneNumber'])) {$phone = $_POST['phoneNumber'];}
+
+	$type = "Заявка на бронирование";
+	/* Сюда впишите свою эл. почту */
+	$address = "info@discoverytour.kz";
+
+	/* А здесь прописывается текст сообщения, \n - перенос строки */
+	$mes = "Тема: $type\nТелефон: $phone\nИмя: $name";
+
+	/* А эта функция как раз занимается отправкой письма на указанный вами email */
+	$sub='Заявка бронирование '; //сабж
+	$email='<discoverytour.kz>'; // от кого
+	$send = mail ($address,$sub,$mes,"Content-type:text/plain; charset = utf-8\r\nFrom:$email");
+	header("location:/");
+}
+if(isset($_POST['OrderToCall'])){
+ /* Здесь проверяется существование переменных */
+	if (isset($_POST['phoneNumber'])) {$phone = $_POST['phoneNumber'];}
+	if (isset($_POST['userName'])) {$name = $_POST['userName'];}
+
+	$type = "Заявка на звонок";
+	/* Сюда впишите свою эл. почту */
+	 $address = "info@discoverytour.kz";
+
+	/* А здесь прописывается текст сообщения, \n - перенос строки */
+	$mes = "Тема: $type\nТелефон: $phone\nИмя: $name";
+
+	/* А эта функция как раз занимается отправкой письма на указанный вами email */
+	$sub='Заявка на звонок'; //сабж
+	$email='<discoverytour.kz>'; // от кого
+	$send = mail ($address,$sub,$mes,"Content-type:text/plain; charset = utf-8\r\nFrom:$email");
+		header("location:/");
+}
+if(isset($_POST['GetConsult'])){
+	/* Здесь проверяется существование переменных */
+	if (isset($_POST['phoneNumber'])) {$phone = $_POST['phoneNumber'];}
+	if (isset($_POST['name'])) {$name = $_POST['name'];}
+
+	$type = "Получить консультацию по зонам отдыха";
+	/* Сюда впишите свою эл. почту */
+	$address = "info@discoverytour.kz";
+
+	/* А здесь прописывается текст сообщения, \n - перенос строки */
+	$mes = "Тема: $type\nТелефон: $phone\nИмя: $name";
+
+	/* А эта функция как раз занимается отправкой письма на указанный вами email */
+	$sub="Получить консультацию по зонам отдыха"; //сабж
+	$email='<discoverytour.kz>'; // от кого
+	$send = mail ($address,$sub,$mes,"Content-type:text/plain; charset = utf-8\r\nFrom:$email");
+	header("location:/");
+}
+if(isset($_POST['KnowMore'])){
+	/* Здесь проверяется существование переменных */
+	if (isset($_POST['phoneNumber'])) {$phone = $_POST['phoneNumber'];}
+	if (isset($_POST['name'])) {$name = $_POST['name'];}
+
+	$type = "Узнать подробнее по зонам отдыха";
+	/* Сюда впишите свою эл. почту */
+	$address = "info@discoverytour.kz";
+
+	/* А здесь прописывается текст сообщения, \n - перенос строки */
+	$mes = "Тема: $type\nТелефон: $phone\nИмя: $name";
+
+	/* А эта функция как раз занимается отправкой письма на указанный вами email */
+	$sub='Узнать подробнее по зонам отдыха'; //сабж
+	$email='<discoverytour.kz>'; // от кого
+	$send = mail ($address,$sub,$mes,"Content-type:text/plain; charset = utf-8\r\nFrom:$email");
+	header("location:/");
+}
+?>
